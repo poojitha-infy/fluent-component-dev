4fa94c48a0f060d2287947f7ddd91fe15db4b6fd
poojitha-srini <poojitha.srinivasan@infosys.com>
2024-08-22 20:05:54 +0530

    removed fluent v2 imports from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,41 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {
-  baseLayerLuminance,
-  StandardLuminance,
-  provideFluentDesignSystem,
-  fluentButton,
-  fluentTextField,
-  fluentCheckbox,
-  fluentRadio,
-  fluentRadioGroup,
-  fluentSwitch,
-  fluentCombobox,
-  fluentOption,
-  } from '@fluentui/web-components';
+// import {
+//   baseLayerLuminance,
+//   StandardLuminance,
+//   provideFluentDesignSystem,
+//   fluentButton,
+//   fluentTextField,
+//   fluentCheckbox,
+//   fluentRadio,
+//   fluentRadioGroup,
+//   fluentSwitch,
+//   fluentCombobox,
+//   fluentOption,
+//   } from '@fluentui/web-components';
 
 
-provideFluentDesignSystem().register(fluentButton(), fluentTextField());
-provideFluentDesignSystem()
-    .register(
-        fluentCheckbox(),
-        fluentRadio(),
-        fluentRadioGroup(),
-        fluentSwitch(),
-        fluentCombobox(),
-        fluentOption(),
+// provideFluentDesignSystem().register(fluentButton(), fluentTextField());
+// provideFluentDesignSystem()
+//     .register(
+//         fluentCheckbox(),
+//         fluentRadio(),
+//         fluentRadioGroup(),
+//         fluentSwitch(),
+//         fluentCombobox(),
+//         fluentOption(),
         
-    );
+//     );
  
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-  const element = document.getElementById('dark');
-  if(element) {
-    baseLayerLuminance.setValueFor(
-      element,
-      StandardLuminance.DarkMode
-    );
-  }
+  // const element = document.getElementById('dark');
+  // if(element) {
+  //   baseLayerLuminance.setValueFor(
+  //     element,
+  //     StandardLuminance.DarkMode
+  //   );
+  // }
